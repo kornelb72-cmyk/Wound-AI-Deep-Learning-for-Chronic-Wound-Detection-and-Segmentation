8341d2d79aa9581c6b1ed057e571b1f98a5652df
kornelb72-cmyk <kornelb72@gmail.com>
2026-02-18 11:15:32 +0100

    Add files via upload

---
+++ DeepLearning/DEMO/visualize_demo.m
@@ -0,0 +1,41 @@
+clear; clc; close all;
+
+rootFolder ='C:/Users/korne/Desktop/DEMO';  
+
+modelFile = fullfile(rootFolder,'deeplabv3plus_resnet50_case_6_1_1_demo.mat');
+imgPath = fullfile(rootFolder,'case_6_1_1_rgb.png');
+
+classNames = ["background","wound"];
+labelIDs = [1 2];
+inputSize = [224 224 3];
+
+S = load(modelFile);  
+net = S.net;
+
+I = readAndPreprocessImage(imgPath,inputSize);
+C = semanticseg(I,net);
+
+predMask = uint8(C);
+predMask(C == "background") = 1;
+predMask(C == "wound") = 2;
+
+B = labeloverlay(I, C,'IncludedLabels',"wound",'Transparency',0.5);
+overlayFile = fullfile(rootFolder,'case_6_1_1_overlay.png');
+imwrite(B,overlayFile);
+fprintf('Overlay was saved in: %s\n',overlayFile);
+
+figure;
+imshow(B);
+title('Prediction');
+
+maskWound = (C == "wound");
+predMaskGray = uint8(maskWound) * 255;
+imwrite(predMaskGray, fullfile(rootFolder,'case_6_1_1_pred_mask_gray.png'));
+
+function I = readAndPreprocessImage(filename, inputSize)
+    I = imread(filename);
+    if size(I,3) == 1
+        I = repmat(I, [1 1 3]);
+    end
+    I = imresize(I, inputSize(1:2));
+end
